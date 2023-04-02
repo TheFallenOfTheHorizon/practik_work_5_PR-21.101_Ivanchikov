704864d7889b285c8caee77f6b5429f1a2db1767
Fallen <0_the_fallen_0@mail.ru>
2023-04-02 17:27:45 +0700

    ses2-1

---
+++ app/src/main/java/com/example/practik_work_5_pr_21101_ivanchikov/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.example.practik_work_5_pr_21101_ivanchikov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
